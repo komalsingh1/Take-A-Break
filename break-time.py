f23234d7d99030fdf42f7d54cc05a3422e7d41c2
Komal Singh <komal17354@gmail.com>
2018-10-26 19:27:05 +0530

    Add files via upload

---
+++ break-time.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+c=5;
+a=0;
+print("This program started"+time.ctime())
+while(a<c):
+    
+    time.sleep(7200)
+    webbrowser.open("https://www.youtube.com/watch?v=uQqkbHfEPho")
+    a=a+1
